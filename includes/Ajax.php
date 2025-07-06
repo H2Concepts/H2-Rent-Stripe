0fa08d0a845e658ac4d8f137c90dea2396b3865c
H2Concepts <hello@h2concepts.de>
2025-07-06 22:08:28 +0200

    Fix AGB link text encoding

--- includes/Ajax.php
+++ includes/Ajax.php
@@ -903,7 +903,7 @@ function federwiegen_create_checkout_session() {
             ],
             'custom_text' => [
                 'terms_of_service_acceptance' => [
-                    'message' => 'Ich akzeptiere die <a href="' . esc_url($tos_url) . '" target="_blank">Allgemeinen Gesch\xC3\xA4ftsbedingungen (AGB)</a>.',
+                    'message' => 'Ich akzeptiere die <a href="' . esc_url($tos_url) . '" target="_blank">Allgemeinen Geschäftsbedingungen (AGB)</a>.',
                 ],
             ],
         ];
--- includes/Plugin.php
+++ includes/Plugin.php
@@ -348,7 +348,7 @@ class Plugin {
                 ],
                 'custom_text' => [
                     'terms_of_service_acceptance' => [
-                        'message' => 'Ich akzeptiere die <a href="' . esc_url($tos_url) . '" target="_blank">Allgemeinen Gesch\xC3\xA4ftsbedingungen (AGB)</a>.',
+                        'message' => 'Ich akzeptiere die <a href="' . esc_url($tos_url) . '" target="_blank">Allgemeinen Geschäftsbedingungen (AGB)</a>.',
                     ],
                 ],
             ];
