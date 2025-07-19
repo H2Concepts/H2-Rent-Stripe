57461983a7e9d0d1bcade0396479c2cba86ed755
H2Concepts <hello@h2concepts.de>
2025-07-20 00:21:01 +0200

    Use full width for Stripe container

--- templates/checkout-page.php
+++ templates/checkout-page.php
@@ -7,7 +7,7 @@ get_header();
 ?>
 <div class="produkt-container shop-overview-container">
     <h1>Checkout</h1>
-    <div id="stripe-container" style="max-width: 700px; margin: 0 auto;">
+    <div id="stripe-container" style="min-width: 100%; margin: 0 auto;">
         <?php echo do_shortcode('[stripe_elements_form]'); ?>
     </div>
 </div>
