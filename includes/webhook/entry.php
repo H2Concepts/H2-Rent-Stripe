041a01028e3c603f9c2d0db48fd2a5e4b1677c39
H2Concepts <hello@h2concepts.de>
2025-07-28 10:17:27 +0200

    Delete includes directory

--- includes/webhook/entry.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Datei: /includes/webhook/entry.php
-
-// Beende HTTP-Verbindung sofort
-ignore_user_abort(true);
-
-register_shutdown_function(function () {
-    require __DIR__ . '/handler.php';
-});
