985af8d3fd4caf10f03e2ab6ce38d090f333c6dc
H2Concepts <hello@h2concepts.de>
2025-08-03 11:04:41 +0200

    Refactor filter admin actions as buttons

--- admin/filters-page.php
+++ admin/filters-page.php
@@ -76,15 +76,15 @@ $options = $current_group ? $wpdb->get_results($wpdb->prepare("SELECT * FROM {$t
 
     <div class="dashboard-grid">
         <div class="dashboard-left">
-            <div class="h2-rental-card">
+            <div class="dashboard-card">
                 <div class="card-header-flex">
                     <div>
                         <h2>Alle Filter Kategorien</h2>
                         <p class="card-subline">Übersicht der Filtergruppen</p>
                     </div>
-                    <a href="<?php echo admin_url('admin.php?page=produkt-filters&add_group=1'); ?>" class="icon-btn add-category-btn" aria-label="Hinzufügen">
+                    <button type="button" class="icon-btn add-category-btn" aria-label="Hinzufügen" onclick="window.location.href='<?php echo admin_url('admin.php?page=produkt-filters&add_group=1'); ?>'">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 80.3"><path d="M12.1,12c-15.4,15.4-15.4,40.4,0,55.8,7.7,7.7,17.7,11.7,27.9,11.7s20.2-3.8,27.9-11.5c15.4-15.4,15.4-40.4,0-55.8-15.4-15.6-40.4-15.6-55.8-.2h0ZM62.1,62c-12.1,12.1-31.9,12.1-44.2,0-12.1-12.1-12.1-31.9,0-44.2,12.1-12.1,31.9-12.1,44.2,0,12.1,12.3,12.1,31.9,0,44.2Z"/><path d="M54.6,35.7h-10.4v-10.4c0-2.3-1.9-4.2-4.2-4.2s-4.2,1.9-4.2,4.2v10.4h-10.4c-2.3,0-4.2,1.9-4.2,4.2s1.9,4.2,4.2,4.2h10.4v10.4c0,2.3,1.9,4.2,4.2,4.2s4.2-1.9,4.2-4.2v-10.4h10.4c2.3,0,4.2-1.9,4.2-4.2s-1.9-4.2-4.2-4.2Z"/></svg>
-                    </a>
+                    </button>
                 </div>
                 <table class="activity-table">
                     <thead><tr><th>Titel</th><th>Aktion</th></tr></thead>
@@ -120,17 +120,17 @@ $options = $current_group ? $wpdb->get_results($wpdb->prepare("SELECT * FROM {$t
                                 <tr>
                                     <td><?php echo esc_html($g->name); ?></td>
                                     <td>
-                                        <a href="<?php echo admin_url('admin.php?page=produkt-filters&group=' . $g->id); ?>" class="icon-btn" aria-label="Optionen">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 80.3"><path d="M12.1,12c-15.4,15.4-15.4,40.4,0,55.8,7.7,7.7,17.7,11.7,27.9,11.7s20.2-3.8,27.9-11.5c15.4-15.4,15.4-40.4,0-55.8-15.4-15.6-40.4-15.6-55.8-.2h0ZM62.1,62c-12.1,12.1-31.9,12.1-44.2,0-12.1-12.1-12.1-31.9,0-44.2,12.1-12.1,31.9-12.1,44.2,0,12.1,12.3,12.1,31.9,0,44.2Z"/><path d="M54.6,35.7h-10.4v-10.4c0-2.3-1.9-4.2-4.2-4.2s-4.2,1.9-4.2,4.2v10.4h-10.4c-2.3,0-4.2,1.9-4.2,4.2s1.9,4.2,4.2,4.2h10.4v10.4c0,2.3,1.9,4.2,4.2,4.2s4.2-1.9,4.2-4.2v-10.4h10.4c2.3,0,4.2-1.9,4.2-4.2s-1.9-4.2-4.2-4.2Z"/></svg>
-                                        </a>
-                                        <a href="<?php echo admin_url('admin.php?page=produkt-filters&edit_group=' . $g->id); ?>" class="icon-btn" aria-label="Bearbeiten">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80.8 80.1"><path d="M54.7,4.8l-31.5,31.7c-.6.6-1,1.5-1.2,2.3l-3.3,18.3c-.2,1.2.2,2.7,1.2,3.8.8.8,1.9,1.2,2.9,1.2h.8l18.3-3.3c.8-.2,1.7-.6,2.3-1.2l31.7-31.7c5.8-5.8,5.8-15.2,0-21-6-5.8-15.4-5.8-21.2,0h0ZM69.9,19.8l-30.8,30.8-11,1.9,2.1-11.2,30.6-30.6c2.5-2.5,6.7-2.5,9.2,0,2.5,2.7,2.5,6.7,0,9.2Z"/><path d="M5.1,79.6h70.8c2.3,0,4.2-1.9,4.2-4.2v-35.4c0-2.3-1.9-4.2-4.2-4.2s-4.2,1.9-4.2,4.2v31.2H9.2V8.8h31.2c2.3,0,4.2-1.9,4.2-4.2s-1.9-4.2-4.2-4.2H5.1c-2.3,0-4.2,1.9-4.2,4.2v70.8c0,2.3,1.9,4.2,4.2,4.2h0Z"/></svg>
-                                        </a>
-                                        <a href="<?php echo admin_url('admin.php?page=produkt-filters&delete_group=' . $g->id . '&fw_nonce=' . wp_create_nonce('produkt_admin_action')); ?>" class="icon-btn" onclick="return confirm('Löschen?');" aria-label="Löschen">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 79.9 80.1"><path d="M39.8.4C18,.4.3,18.1.3,40s17.7,39.6,39.6,39.6,39.6-17.7,39.6-39.6S61.7.4,39.8.4ZM39.8,71.3c-17.1,0-31.2-14-31.2-31.2s14.2-31.2,31.2-31.2,31.2,14,31.2,31.2-14.2,31.2-31.2,31.2Z"/><path d="M53,26.9c-1.7-1.7-4.2-1.7-5.8,0l-7.3,7.3-7.3-7.3c-1.7-1.7-4.2-1.7-5.8,0-1.7,1.7-1.7,4.2,0,5.8l7.3,7.3-7.3,7.3c-1.7,1.7-1.7,4.2,0,5.8.8.8,1.9,1.2,2.9,1.2s2.1-.4,2.9-1.2l7.3-7.3,7.3,7.3c.8.8,1.9,1.2,2.9,1.2s2.1-.4,2.9-1.2c1.7-1.7,1.7-4.2,0-5.8l-7.3-7.3,7.3-7.3c1.7-1.7,1.7-4.4,0-5.8h0Z"/></svg>
-                                        </a>
-                                    </td>
-                                </tr>
+                                    <button type="button" class="icon-btn" aria-label="Optionen" onclick="window.location.href='<?php echo admin_url('admin.php?page=produkt-filters&group=' . $g->id); ?>'">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 80.3"><path d="M12.1,12c-15.4,15.4-15.4,40.4,0,55.8,7.7,7.7,17.7,11.7,27.9,11.7s20.2-3.8,27.9-11.5c15.4-15.4,15.4-40.4,0-55.8-15.4-15.6-40.4-15.6-55.8-.2h0ZM62.1,62c-12.1,12.1-31.9,12.1-44.2,0-12.1-12.1-12.1-31.9,0-44.2,12.1-12.1,31.9-12.1,44.2,0,12.1,12.3,12.1,31.9,0,44.2Z"/><path d="M54.6,35.7h-10.4v-10.4c0-2.3-1.9-4.2-4.2-4.2s-4.2,1.9-4.2,4.2v10.4h-10.4c-2.3,0-4.2,1.9-4.2,4.2s1.9,4.2,4.2,4.2h10.4v10.4c0,2.3,1.9,4.2,4.2,4.2s4.2-1.9,4.2-4.2v-10.4h10.4c2.3,0,4.2-1.9,4.2-4.2s-1.9-4.2-4.2-4.2Z"/></svg>
+                                    </button>
+                                    <button type="button" class="icon-btn" aria-label="Bearbeiten" onclick="window.location.href='<?php echo admin_url('admin.php?page=produkt-filters&edit_group=' . $g->id); ?>'">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80.8 80.1"><path d="M54.7,4.8l-31.5,31.7c-.6.6-1,1.5-1.2,2.3l-3.3,18.3c-.2,1.2.2,2.7,1.2,3.8.8.8,1.9,1.2,2.9,1.2h.8l18.3-3.3c.8-.2,1.7-.6,2.3-1.2l31.7-31.7c5.8-5.8,5.8-15.2,0-21-6-5.8-15.4-5.8-21.2,0h0ZM69.9,19.8l-30.8,30.8-11,1.9,2.1-11.2,30.6-30.6c2.5-2.5,6.7-2.5,9.2,0,2.5,2.7,2.5,6.7,0,9.2Z"/><path d="M5.1,79.6h70.8c2.3,0,4.2-1.9,4.2-4.2v-35.4c0-2.3-1.9-4.2-4.2-4.2s-4.2,1.9-4.2,4.2v31.2H9.2V8.8h31.2c2.3,0,4.2-1.9,4.2-4.2s-1.9-4.2-4.2-4.2H5.1c-2.3,0-4.2,1.9-4.2,4.2v70.8c0,2.3,1.9,4.2,4.2,4.2h0Z"/></svg>
+                                    </button>
+                                    <button type="button" class="icon-btn" aria-label="Löschen" onclick="if(confirm('Löschen?')){window.location.href='<?php echo admin_url('admin.php?page=produkt-filters&delete_group=' . $g->id . '&fw_nonce=' . wp_create_nonce('produkt_admin_action')); ?>';}">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 79.9 80.1"><path d="M39.8.4C18,.4.3,18.1.3,40s17.7,39.6,39.6,39.6,39.6-17.7,39.6-39.6S61.7.4,39.8.4ZM39.8,71.3c-17.1,0-31.2-14-31.2-31.2s14.2-31.2,31.2-31.2,31.2,14,31.2,31.2-14.2,31.2-31.2,31.2Z"/><path d="M53,26.9c-1.7-1.7-4.2-1.7-5.8,0l-7.3,7.3-7.3-7.3c-1.7-1.7-4.2-1.7-5.8,0-1.7,1.7-1.7,4.2,0,5.8l7.3,7.3-7.3,7.3c-1.7,1.7-1.7,4.2,0,5.8.8.8,1.9,1.2,2.9,1.2s2.1-.4,2.9-1.2l7.3-7.3,7.3,7.3c.8.8,1.9,1.2,2.9,1.2s2.1-.4,2.9-1.2c1.7-1.7,1.7-4.2,0-5.8l-7.3-7.3,7.3-7.3c1.7-1.7,1.7-4.4,0-5.8h0Z"/></svg>
+                                    </button>
+                                </td>
+                            </tr>
                             <?php endif; ?>
                         <?php endforeach; ?>
                     </tbody>
@@ -138,16 +138,16 @@ $options = $current_group ? $wpdb->get_results($wpdb->prepare("SELECT * FROM {$t
             </div>
         </div>
         <div class="dashboard-right">
-            <div class="h2-rental-card">
+            <div class="dashboard-card">
                 <div class="card-header-flex">
                     <div>
                         <h2>Filteroptionen</h2>
                         <p class="card-subline">Fügen Sie Filter hinzu</p>
                     </div>
                     <?php if ($current_group): ?>
-                        <a href="<?php echo admin_url('admin.php?page=produkt-filters&group=' . $current_group->id . '&add_option=1'); ?>" class="icon-btn add-category-btn" aria-label="Hinzufügen">
+                        <button type="button" class="icon-btn add-category-btn" aria-label="Hinzufügen" onclick="window.location.href='<?php echo admin_url('admin.php?page=produkt-filters&group=' . $current_group->id . '&add_option=1'); ?>'">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 80.3"><path d="M12.1,12c-15.4,15.4-15.4,40.4,0,55.8,7.7,7.7,17.7,11.7,27.9,11.7s20.2-3.8,27.9-11.5c15.4-15.4,15.4-40.4,0-55.8-15.4-15.6-40.4-15.6-55.8-.2h0ZM62.1,62c-12.1,12.1-31.9,12.1-44.2,0-12.1-12.1-12.1-31.9,0-44.2,12.1-12.1,31.9-12.1,44.2,0,12.1,12.3,12.1,31.9,0,44.2Z"/><path d="M54.6,35.7h-10.4v-10.4c0-2.3-1.9-4.2-4.2-4.2s-4.2,1.9-4.2,4.2v10.4h-10.4c-2.3,0-4.2,1.9-4.2,4.2s1.9,4.2,4.2,4.2h10.4v10.4c0,2.3,1.9,4.2,4.2,4.2s4.2-1.9,4.2-4.2v-10.4h10.4c2.3,0,4.2-1.9,4.2-4.2s-1.9-4.2-4.2-4.2Z"/></svg>
-                        </a>
+                        </button>
                     <?php endif; ?>
                 </div>
                 <table class="activity-table">
@@ -187,12 +187,12 @@ $options = $current_group ? $wpdb->get_results($wpdb->prepare("SELECT * FROM {$t
                                     <tr>
                                         <td><?php echo esc_html($opt->name); ?></td>
                                         <td>
-                                            <a href="<?php echo admin_url('admin.php?page=produkt-filters&group=' . $current_group->id . '&edit_option=' . $opt->id); ?>" class="icon-btn" aria-label="Bearbeiten">
+                                            <button type="button" class="icon-btn" aria-label="Bearbeiten" onclick="window.location.href='<?php echo admin_url('admin.php?page=produkt-filters&group=' . $current_group->id . '&edit_option=' . $opt->id); ?>'">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80.8 80.1"><path d="M54.7,4.8l-31.5,31.7c-.6.6-1,1.5-1.2,2.3l-3.3,18.3c-.2,1.2.2,2.7,1.2,3.8.8.8,1.9,1.2,2.9,1.2h.8l18.3-3.3c.8-.2,1.7-.6,2.3-1.2l31.7-31.7c5.8-5.8,5.8-15.2,0-21-6-5.8-15.4-5.8-21.2,0h0ZM69.9,19.8l-30.8,30.8-11,1.9,2.1-11.2,30.6-30.6c2.5-2.5,6.7-2.5,9.2,0,2.5,2.7,2.5,6.7,0,9.2Z"/><path d="M5.1,79.6h70.8c2.3,0,4.2-1.9,4.2-4.2v-35.4c0-2.3-1.9-4.2-4.2-4.2s-4.2,1.9-4.2,4.2v31.2H9.2V8.8h31.2c2.3,0,4.2-1.9,4.2-4.2s-1.9-4.2-4.2-4.2H5.1c-2.3,0-4.2,1.9-4.2,4.2v70.8c0,2.3,1.9,4.2,4.2,4.2h0Z"/></svg>
-                                            </a>
-                                            <a href="<?php echo admin_url('admin.php?page=produkt-filters&group=' . $current_group->id . '&delete_option=' . $opt->id . '&fw_nonce=' . wp_create_nonce('produkt_admin_action')); ?>" class="icon-btn" onclick="return confirm('Löschen?');" aria-label="Löschen">
+                                            </button>
+                                            <button type="button" class="icon-btn" aria-label="Löschen" onclick="if(confirm('Löschen?')){window.location.href='<?php echo admin_url('admin.php?page=produkt-filters&group=' . $current_group->id . '&delete_option=' . $opt->id . '&fw_nonce=' . wp_create_nonce('produkt_admin_action')); ?>';}">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 79.9 80.1"><path d="M39.8.4C18,.4.3,18.1.3,40s17.7,39.6,39.6,39.6,39.6-17.7,39.6-39.6S61.7.4,39.8.4ZM39.8,71.3c-17.1,0-31.2-14-31.2-31.2s14.2-31.2,31.2-31.2,31.2,14,31.2,31.2-14.2,31.2-31.2,31.2Z"/><path d="M53,26.9c-1.7-1.7-4.2-1.7-5.8,0l-7.3,7.3-7.3-7.3c-1.7-1.7-4.2-1.7-5.8,0-1.7,1.7-1.7,4.2,0,5.8l7.3,7.3-7.3,7.3c-1.7,1.7-1.7,4.2,0,5.8.8.8,1.9,1.2,2.9,1.2s2.1-.4,2.9-1.2l7.3-7.3,7.3,7.3c.8.8,1.9,1.2,2.9,1.2s2.1-.4,2.9-1.2c1.7-1.7,1.7-4.2,0-5.8l-7.3-7.3,7.3-7.3c1.7-1.7,1.7-4.4,0-5.8h0Z"/></svg>
-                                            </a>
+                                            </button>
                                         </td>
                                     </tr>
                                 <?php endif; ?>
